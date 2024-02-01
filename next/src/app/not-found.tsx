56ec541040e28a6f0238bc4193be9272045ce95c
Tyler Maksymiw <tylermcodes@gmail.com>
2024-01-31 22:27:04 -0500

    Add next.js and remove react

---
+++ next/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => { 
+    return(
+        <div>
+            <p>Not found</p>
+        </div>
+    )
+}
+
+export default NotFound
---
+++ next/src/components/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+const NavBar = () => { 
+    return ( 
+        <nav>
+
+        </nav>
+    )
+    
+}
+
+export default NavBar
